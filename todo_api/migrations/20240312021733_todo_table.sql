f0fadbacd45bf9f8cb4550b4487a697740e7ff37
René Delion <therealschizzo@gmail.com>
2024-03-12 14:06:37 +0100

    Add Category Foreign Key to todo_table

--- todo_api/migrations/20240312021733_todo_table.sql
+++ todo_api/migrations/20240312021733_todo_table.sql
@@ -3,5 +3,6 @@ CREATE TABLE todo_table (
   todo_id UUID PRIMARY KEY NOT NULL UNIQUE,
   todo_name TEXT NOT NULL,
   id_done BOOLEAN NOT NULL,
-  user_id UUID REFERENCES user_table(user_id) ON DELETE CASCADE
+  user_id UUID REFERENCES user_table(user_id) ON DELETE CASCADE,
+  category_id UUID REFERENCES category_table(category_id) ON DELETE CASCADE
 );
