e55e9cca29262f9f66b24c2f08cdcf358bacadd7
René Delion <therealschizzo@gmail.com>
2024-03-12 06:17:24 +0100

    Update Migration

--- todo_api/migrations/20240312021653_user_table.sql
+++ todo_api/migrations/20240312021653_user_table.sql
@@ -1,6 +1,6 @@
 -- Add migration script here
 CREATE TABLE user_table (
   user_id UUID PRIMARY KEY NOT NULL UNIQUE,
-  user_name TEXT NOT NULL,
+  user_name TEXT NOT NULL UNIQUE,
   password TEXT NOT NULL
 );
