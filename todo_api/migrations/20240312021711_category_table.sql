4f518d1c83c58413e09c8446c7df55a603306482
René Delion <therealschizzo@gmail.com>
2024-03-12 16:25:59 +0100

    Fixed Unique Modifier in Category

--- todo_api/migrations/20240312021711_category_table.sql
+++ todo_api/migrations/20240312021711_category_table.sql
@@ -2,5 +2,6 @@
 CREATE TABLE category_table (
   category_id UUID PRIMARY KEY NOT NULL UNIQUE,
   category_name TEXT NOT NULL,
-  user_id UUID REFERENCES user_table(user_id) ON DELETE CASCADE
+  user_id UUID REFERENCES user_table(user_id) ON DELETE CASCADE,
+  UNIQUE (user_id, category_name)
 );    
