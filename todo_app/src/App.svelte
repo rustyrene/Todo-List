aa7745839b810b5a719618daf798ec7de936654d
rustyrene <rene.delion.buisness@protonmail.com>
2024-03-13 09:04:34 +0100

    Add basic layout

--- todo_app/src/App.svelte
+++ todo_app/src/App.svelte
@@ -1,4 +1,28 @@
-<main>
-  <h1>Coming soon...</h1>
-</main>
+<script>
+  import { onMount } from "svelte";
+  import Router from "svelte-spa-router";
+  import { push } from "svelte-spa-router";
+  import Login from "./Login.svelte";
+  import Main from "./Main.svelte";
+  import axios from "axios";
 
+  const routes = {
+    '/login': Login,
+    '/home': Main,
+  }
+
+  //!TODO Check if user has valid cookie and redirect accordingly
+  onMount(async () => {
+    //Check if token is still valid
+    const response = await axios.get("http://localhost:3000/token-check", {withCredentials:true});
+    console.log(response)
+    
+    if (response.status === 200) {
+      push("/home");
+    } else {
+      push("/login");
+    }
+  })
+</script>
+
+<Router {routes} />
---
+++ todo_app/src/Login.svelte
@@ -0,0 +1,20 @@
+<script>
+  import axios from "axios";
+    import { replace } from "svelte-spa-router";
+
+    let username = "test";
+    let password = "test";
+
+    const login = async () => {
+        const response = await axios.post("http://localhost:3000/user/login", { 
+            "username": username, "password":password 
+        }, { 
+            withCredentials: true
+        });
+        console.log(response);
+    }
+</script>
+
+<main>
+    <button on:click={login}>Login</button>
+</main>
