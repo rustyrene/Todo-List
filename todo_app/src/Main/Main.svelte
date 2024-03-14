a444fc843293ab60e065895e014903494b31d1c6
rustyrene <rene.delion.buisness@protonmail.com>
2024-03-14 15:48:43 +0100

    Improved Main Page

---
+++ todo_app/src/Main/Main.svelte
@@ -0,0 +1,151 @@
+<script>
+    import axios from "axios";
+    import { pop, replace } from "svelte-spa-router";
+    import CategoryModal from "./category-modal.svelte";
+
+    let popUp = false;
+
+    const reload = () => {
+        window.location.reload();
+    }
+
+    const logout = async () => {
+        try {
+            await axios.get("http://localhost:3000/user/logout", {
+                withCredentials:true
+            });
+            replace("/login")
+        } catch(err) {
+            replace("/login")
+        }
+    } 
+
+    const modal = () => {
+        popUp = !popUp;
+    }
+</script>
+
+{#if popUp}
+    <CategoryModal closeModal={modal} />
+{/if}
+<main>
+    <nav>
+        <button></button>
+        <button class="headline" on:click={reload}>Task IT</button>
+        <button class="logout" on:click={logout}>Logout</button>
+    </nav>
+    <div class="wrapper">
+        <div class="sidebar">
+            <ul>
+                <li><button>Overview</button></li>
+                <li><button on:click={() => {popUp=true}}><i class="fa-solid fa-circle-plus"></i></button></li>
+            </ul>
+        </div>
+        <div class="content">
+        </div>
+    </div>
+</main> 
+
+<style>
+    main {
+        min-height: 100vh;
+    }
+
+    nav {
+        width: 100%;
+        height: 60px;
+
+        display: flex;
+        position: fixed;
+        top: 0;
+        justify-content: space-between;
+        align-items: center;
+
+        margin-top: 0 !important;
+
+        background-color: var(--primary);
+        box-shadow: 0 5px 5px rgba(0, 0, 0, .2);
+    }
+
+    nav button {
+        border: none;
+        background-color: transparent;
+        cursor: pointer;
+    }
+
+    nav button.headline {
+        font-size: 36px;
+        font-weight: 600;
+        color: rgba(255, 255, 255, 1);
+    }
+
+    nav button.logout {
+        margin-right: 46px;
+        font-size: 26px;
+        color: rgba(255, 255, 255, .8);
+        transition: .1s ease-out;
+    }
+
+    nav button.logout:hover {
+        color: white;
+    }
+
+    .wrapper {
+        display: flex;
+        min-height: 100vh;
+        width: 100%;
+        margin-top: 60px;
+    }
+
+    .sidebar {
+        width: 300px;
+        height: 100%;
+        position: fixed;
+
+        background-color: var(--primary-dark);
+        box-shadow: 3px 0 5px rgba(0, 0, 0, .2);
+
+        z-index: 50;
+    }
+
+    .sidebar ul {
+        margin-top: 36px;
+    }
+
+    .sidebar ul li {
+        width: 100%;
+        height: 54px;
+        margin-top: 4px;
+    }
+
+    .sidebar ul li button {
+        width: 100%;
+        padding: 10px 0;
+
+        text-decoration: none;
+        font-size: 26px;
+        color: rgba(255, 255, 255, .8);
+        text-align: center;
+
+        background-color: transparent;
+        border: none;
+        
+        transition: .1s ease-out;
+        cursor: pointer;
+    }
+
+    .sidebar ul li button:hover {
+        background-color: var(--primary-light);
+    }
+
+    .sidebar ul li button i {
+        font-size: 22px;
+    }
+
+    .content {
+        flex: 1;
+        position: fixed;
+        margin-left: 300px;
+        background-color: var(--background);
+    }
+</style>
---
+++ todo_app/src/Main/category-modal.svelte
@@ -0,0 +1,68 @@
+<script>
+    import { onDestroy, onMount } from "svelte";
+
+    export let closeModal;
+
+    onMount(() => {
+        const body = document.querySelector("body");
+        window.addEventListener("keydown", closeModal)
+        body.style.overflow = "hidden";
+    });
+
+    onDestroy(() => {
+        const body = document.querySelector("body");
+        body.style.overflow = "";
+    })
+</script>
+
+<div class="modal" on:click={closeModal} on:keydown={closeModal} role="button" tabindex="-1">
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <div class="box" id="box" on:click={(event) => event.stopPropagation()} role="button" tabindex="0" on:keydown={closeModal}>
+        <div class="controls">
+            <i></i>
+            <h2>Add Category</h2>
+            <i class="fa-solid fa-xmark"></i>
+        </div>
+        <p>Do you want to add a new category?</p>
+        <div class="input">
+            <label for="category-name">Category:</label>
+            <input type="text" id="category-name">
+        </div>
+    </div>
+</div>
+
+<style>
+    .modal {
+        position: absolute;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        left: 0;
+        top: 0;
+        margin-top: 60px;
+
+        height: 100vh;
+        width: 100%;
+
+        background-color: rgba(0, 0, 0, .2);
+        z-index: 500;
+    }
+
+    .box {
+        height: 100px;
+        width: 400px;
+
+        display: flex;
+        flex-direction: column;
+        padding: 10px 30px;
+        
+        border-radius: 20px;
+        background-color: var(--foreground);
+        z-index: 1000;
+    }
+
+    .box .controls {
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
